19e67464eb523687730eecd2cb9343955e1fe12a
Avi Israeli <aviv8710@gmail.com>
2024-06-05 22:39:40 +0300

    mid project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ColorList from "./ColorList";
+import Form from "./Form";
+import Values from "values.js";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [colors, setColors] = useState(new Values("#f15025").all(10));
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <main>
+      <Form />
+      <ColorList colors={colors} />
+    </main>
+  );
+};
+export default App;
---
+++ src/Form.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [color, setColor] = useState("");
+
+  const handleChange = (e) => {
+    setColor(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+  return (
+    <section className="container">
+      <h4>color generator</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input type="color" value={color} onChange={handleChange} />
+        <input
+          type="text"
+          value={color}
+          onChange={handleChange}
+          placeholder="#f15025"
+        />
+        <button className="btn" type="submit" style={{ background: color }}>
+          submit color
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
